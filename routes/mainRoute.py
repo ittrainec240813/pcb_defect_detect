4da0fff2ae170e1bec9be59f6590a1a492a59527
Luke3501 <lukechu3501@gmail.com>
2024-10-21 09:58:06 +0800

    update predict param

--- routes/mainRoute.py
+++ routes/mainRoute.py
@@ -2,7 +2,7 @@ import base64, io, json, os
 from PIL import Image
 from dotenv import load_dotenv
 
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
 from fastapi import Request,  Header, BackgroundTasks, HTTPException, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -23,11 +23,11 @@ async def test():
     if not os.path.exists("./pcb_defect_detect/val"):
         return {"msg": "test file doesn't exist"}
     image = Image.open("./pcb_defect_detect/val/0014089.jpg")
-    results = model.predict(source=image, conf=0.5)
+    results = model.predict(source=image)
     return {"results": json.loads(results[0].to_json())}
 
 @router.post("/inference")
 async def test_image_upload(body: ImageUploadBody):
     image = Image.open(io.BytesIO(base64.b64decode(body.image)))
-    results = model.predict(source=image, conf=0.5)
+    results = model.predict(source=image)
     return {"results": json.loads(results[0].to_json())}
