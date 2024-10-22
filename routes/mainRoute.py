4b4e0ea3ab52ca8555dc8bf9629d0fdc6158e153
Luke3501 <lukechu3501@gmail.com>
2024-10-22 11:01:22 +0800

    add websocket chat

--- routes/mainRoute.py
+++ routes/mainRoute.py
@@ -2,11 +2,12 @@ import base64, io, json, os
 from PIL import Image
 from dotenv import load_dotenv
 
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from fastapi import Request,  Header, BackgroundTasks, HTTPException, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
+import google.generativeai as genai
 from ultralytics import YOLO
 
 load_dotenv()
@@ -14,6 +15,10 @@ load_dotenv()
 router = APIRouter()
 
 model = YOLO("./pcb_defect_detect/pcb_defect_detect.pt")
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+chat_model = genai.GenerativeModel('gemini-1.5-flash')
+generation_config = genai.types.GenerationConfig(temperature=0.2, top_p=0.5, top_k=16)
 
 class ImageUploadBody(BaseModel):
     image: str
@@ -31,3 +36,28 @@ async def test_image_upload(body: ImageUploadBody):
     image = Image.open(io.BytesIO(base64.b64decode(body.image)))
     results = model.predict(source=image)
     return {"results": json.loads(results[0].to_json())}
+
+@router.websocket("/chat")
+async def chat(websocket: WebSocket):
+    await websocket.accept()
+    chat = chat_model.start_chat()
+    
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # await websocket.send_text(f"Message text was: {data}")
+            try:
+                completion = chat.send_message(data, generation_config=generation_config)
+                if (completion.parts[0].text != None):
+                    # 取得生成結果
+                    out = completion.parts[0].text
+                else:
+                    # 回覆 "Gemini沒答案!請換個說法！"
+                    out = "Gemini沒答案!請換個說法！"
+            except Exception as e:
+                print(e)
+                out = "Gemini執行出錯!請換個說法！" 
+
+            await websocket.send_text(out)
+    except WebSocketDisconnect:
+        print("socket disconnected")
